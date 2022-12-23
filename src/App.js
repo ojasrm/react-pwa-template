9453d4c58f2d5ed9fe7d33137fb898f7b9681477
Ojas Mahajan <ojas.rmahajan23@gmail.com>
2022-12-23 18:05:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+        <div className="App-header">
+          <h2>Welcome to React PWA Template</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit the files
+        </p>
+      </div>
+	);
+}
+
+export default App;
